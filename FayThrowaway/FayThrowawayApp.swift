7429370098d5d8177d94917cdafe4f6fc53b4e21
Justin Lee <justinj.lee23@gmail.com>
2025-06-26 13:29:09 -0700

    Initial Commit

---
+++ FayThrowaway/FayThrowawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FayThrowawayApp.swift
+//  FayThrowaway
+//
+//  Created by Justin Lee on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FayThrowawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
